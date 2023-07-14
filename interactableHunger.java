961f9284acfb2c8d000f778d6f5d97e977b732e6
napkin <dave@usednapk.in>
2023-07-14 13:03:10 +0100

    more interactables
    more interactables

---
+++ interactableHunger.java
@@ -0,0 +1,16 @@
+public class interactableHunger extends interactables {
+
+    private final String[] hungerNamePool = new String[]{"Fancy", "Crisp 'n' Ready", "Delicious", "Umami King", "Chef's Choice", "Sous Chef's Delight", "Crunchy"};
+    private final String[] interactableNamePool = new String[]{"Fridge", "Oven", "Cupcake Machine", "Vending Machine", "Toaster", "Microwave", "RoboChef"};
+
+    public interactableHunger() {
+        //effectiveness between 40 and 100
+        this.interactableEffectiveness = randomizer.nextInt(40, 100);
+
+        //between five and ten ingame minutes
+        this.interactableUsageDuration = randomizer.nextInt(5, 10);
+
+        //randomise the name
+        this.interactableName = randomItem(randomizer, hungerNamePool) + " " + randomItem(randomizer, interactableNamePool);
+    }
+}
---
+++ interactableHygiene.java
@@ -0,0 +1,15 @@
+public class interactableHygiene extends interactables {
+    private final String[] hygeineNamePool = new String[]{"Fancy", "Bubbles", "Deluxe", "Stink-B-Gone", "Automatic", "Clean As A Whistle"};
+    private final String[] interactableNamePool = new String[]{"Bath", "Shower", "Cleaning Machine", "Kitchen Sink", "Jacuzzi", "Skin Blaster 9000", "PowerWasher"};
+
+    public interactableHygiene() {
+        //effectiveness between 80 and 100
+        this.interactableEffectiveness = randomizer.nextInt(80, 100);
+
+        //between 20 and 40 ingame minutes
+        this.interactableUsageDuration = randomizer.nextInt(20, 40);
+
+        //randomise the name
+        this.interactableName = randomItem(randomizer, hygeineNamePool) + " " + randomItem(randomizer, interactableNamePool);
+    }
+}
